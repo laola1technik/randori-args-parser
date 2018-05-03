e97338f72e8df1b5b65eb761f5a95ac2bfa1cbb5
Karl Zierhofer <karl.zierhofer@laola1.at>
2018-05-03 13:11:23 +0200

    initial setup

---
+++ tests/Unit/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\Unit;
+
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_succeed()
+    {
+        $this->assertTrue(true);
+    }
+}
