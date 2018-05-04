4bd3a433d437a435a0362954f1721fcf7fab8269
Karl Zierhofer <karl.zierhofer@laola1.at>
2018-05-04 14:43:57 +0200

    Adds system test.

---
+++ tests/Unit/ArgumentsParserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace tests\Unit;
+
+class ArgumentsParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function should_parse_arguments()
+    {
+        $commandLineArguments = '-l -p 8080 -d /usr/logs';
+        $arguments = [
+            new BooleanArgument("l"),
+            new IntegerArgument("p"),
+            new StringArgument("d")
+        ];
+        $scheme = new Scheme($arguments);
+        $argumentParser = new ArgumentParser($scheme);
+
+        $argumentParser->parse($commandLineArguments);
+
+        $this->assertTrue($argumentParser->get("l"));
+        $this->assertSame(8080, $argumentParser->get("p"));
+        $this->assertSame("/usr/logs", $argumentParser->get("d"));
+    }
+}
